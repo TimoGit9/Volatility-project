2725ba6bffe047736e01eee1e4427baaf4b3d9fc
jakubmichalski <51727863@fsv.cuni.cz>
2025-02-02 00:04:27 +0100

    cleaning data

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -1,27 +1,48 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 class DataPreprocessor:
     def __init__(self, datasets={}):
         self.datasets = datasets
         return
 
-    def PrintTest(self):
-        print("Data Preprocessor")
-        #print(self.datasets["VIX"].head())
-    def TestPD(self):
-        print(f"Data Preprocessor", flush=True)
-    def PlotData(self):
-        fig, ax1 = plt.subplots(figsize=(12, 6))
+    def clean_gdp(self):
+        """Cleans and processes the GDP dataset."""
 
-        ax1.set_title("Stock Market Volatility vs. Macroeconomic Indicators")
-        ax1.plot(self.datasets["VIX"].index, self.datasets["VIX"]['CLOSE'], color='blue', label='Volatility')
-        ax1.set_xlabel("Date")
-        ax1.set_ylabel("Volatility", color='blue')
-        ax1.tick_params(axis='y', labelcolor='blue')
+        df = self.datasets.get("GDP")
+        df = df[["Date", "GDP Final*.1"]].copy()
+        df.columns = ["Date", "GDP_Final"]
+        df["GDP_Final"] = pd.to_numeric(df["GDP_Final"], errors="coerce")
+        df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+        df.sort_values("Date", ascending=True, inplace=True)
+        df.set_index("Date", inplace=True)
+        df["GDP_Final"] = df["GDP_Final"].ffill()
+        self.datasets["GDP"] = df
+    def clean_Unemployment(self):
+        """Cleans and processes the SOFR dataset."""
+        df = self.datasets.get("Unemployment")
 
-        fig.legend(loc="upper left", bbox_to_anchor=(0.1, 0.9))
-        plt.show()
+        df = df[["Period", "First Release"]].copy()
+        df.columns = ["Period", "Unemployment_Rate"]
+        df["Date"] = pd.to_datetime(
+            df["Period"].str.extract(r'([A-Za-z]+) (\d{4})').apply(
+                lambda x: x[1] + "-" +
+                          {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06",
+                           "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}[x[0]]
+                          + "-01", axis=1
+            ),
+            format="%Y-%m-%d",
+            errors="coerce"
+        )
+        df.drop(columns=["Period"], inplace=True)
+        df["Unemployment_Rate"] = pd.to_numeric(df["Unemployment_Rate"], errors="coerce")
+        df.sort_values("Date", ascending=True, inplace=True)
+        df.set_index("Date", inplace=True)
+        self.datasets["Unemployment"] = df
 
-    def Show(self, name):
-        print(self.datasets[name].head())
-        return
+
+    def SetSameInterval(self):
+        self.clean_gdp()
+        self.clean_Unemployment()
+
+
 
